bf23e33a00de58bd8baf5fd842eb98ec0eb9b508
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-06-12 12:13:26 -0500

    start code

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var getWeather = function(weather) {
+    var weather = "https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=standard";
+    console.log("Does this work?")
+};
+getWeather();
