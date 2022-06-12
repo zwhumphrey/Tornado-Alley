fcf195bbe768bf5a97f7711ef00ce7e717c2f18e
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-06-12 15:09:16 -0500

    added api key

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
+var apiKey = "16c08d72512cd292161652f7d9f82632";
+
+
+
 var getWeather = function(weather) {
     var weather = "https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=standard";
-    console.log("Does this work?")
+    console.log(weather)
 };
 getWeather();
